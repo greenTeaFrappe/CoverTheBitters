daf83c5853821156adf29c62639a3cbfafd3cb3c
CheHyeonyeong <gusdudco6@naver.com>
2024-03-14 18:22:27 +0900

    ✨ Add music sliender
    음악, 조도 조절 가능 기능 추가

--- On The Land/Assets/Resources/Scripts/gameSettings.cs
+++ On The Land/Assets/Resources/Scripts/gameSettings.cs
@@ -9,37 +9,51 @@ public class gameSettings : MonoBehaviour
     public Slider soundEffectVolumeSlider;
     public Slider brightnessSlider;
 
+    public Text backgroundVolumeText;
+    public Text soundEffectVolumeText;
+    public Text brightnessText;
+
     private const string BackgroundVolumeKey = "BackgroundVolume";
     private const string SoundEffectVolumeKey = "SoundEffectVolume";
     private const string BrightnessKey = "Brightness";
+
 
     void Start()
     {
         // 설정을 불러와서 슬라이더에 반영합니다.
         LoadSettings();
+
         // 슬라이더의 이벤트를 설정합니다.
         backgroundVolumeSlider.onValueChanged.AddListener(delegate { SaveBackgroundVolume(); });
         soundEffectVolumeSlider.onValueChanged.AddListener(delegate { SaveSoundEffectVolume(); });
         brightnessSlider.onValueChanged.AddListener(delegate { SaveBrightness(); });
 
+
+        backgroundVolumeText.text = ((int)(backgroundVolumeSlider.value * 100)).ToString() + "%";
+        soundEffectVolumeText.text = ((int)(soundEffectVolumeSlider.value * 100)).ToString() + "%";
+        brightnessText.text = ((int)(brightnessSlider.value) * 100).ToString() + "%";
     }
 
-
+    
     public void SaveBackgroundVolume()
     {
         PlayerPrefs.SetFloat(BackgroundVolumeKey, backgroundVolumeSlider.value);
+        backgroundVolumeText.text = ((int)(backgroundVolumeSlider.value*100)).ToString()+"%";
         ApplyBackgroundVolume();
     }
 
     public void SaveSoundEffectVolume()
     {
         PlayerPrefs.SetFloat(SoundEffectVolumeKey, soundEffectVolumeSlider.value);
+        soundEffectVolumeText.text = ((int)(soundEffectVolumeSlider.value*100)).ToString()+"%";
+        
         ApplySoundEffectVolume();
     }
 
     public void SaveBrightness()
     {
         PlayerPrefs.SetFloat(BrightnessKey, brightnessSlider.value);
+        brightnessText.text = ((int)(brightnessSlider.value)*100).ToString()+"%";
         ApplyBrightness();
     }
 
