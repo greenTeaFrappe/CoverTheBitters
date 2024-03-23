f27021bd25b41f8df5804c7cc72a86521b6328d4
CheHyeonyeong <gusdudco6@naver.com>
2024-03-23 13:37:11 +0900

    🐛 Automode fix
     auto할 때 stop 하기 전까지 약간 영화관 모드처럼 load, log 보기 이런거 싹 못하게 했음! 마우스 클릭이거나 스페이스바  클릭시 auto 모드 사라짐!!

--- On The Land/Assets/Resources/Scripts/ScriptOutput.cs
+++ On The Land/Assets/Resources/Scripts/ScriptOutput.cs
@@ -9,14 +9,18 @@ public class ScriptOutput : MonoBehaviour
     public Button setB;
 
     public Button logBtn;
-    public Button backBtn;
+    public Button logbackBtn;
     public ScrollRect scrollRect;
 
     public Button autoBtn;
     public Text autoBtnText; // 자동진행 버튼의 텍스트를 변경하기 위한 변수 추가
 
+    public Button loadBtn;
+    public Button loadBackBtn;
+
     public GameObject loadScreen;
     public GameObject saveImgCav;
+
 
     public Button skipBtn;
 
@@ -37,18 +41,32 @@ public class ScriptOutput : MonoBehaviour
     private void autoBtnClick()
     {
         isButtonClicked = !isButtonClicked; // 자동 진행 상태를 토글합니다.
-        //error : save와 log 버튼 클릭해도 자동진행이 됨
+        
         if (isButtonClicked)
         {
+
             autoBtnText.text = "STOP"; // 버튼 텍스트를 "일시정지"로 변경합니다.
+
+            skipBtn.gameObject.SetActive(false);
+            logBtn.gameObject.SetActive(false);
+            loadBtn.gameObject.SetActive(false);
+            
+            // 마우스 클릭 감지
+            if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("space"))
+            {
+                autoBtnClick();
+            }
+
             InvokeRepeating(nameof(HandleMouseClick), 0f, 1f);
         }
         else
         {
+            skipBtn.gameObject.SetActive(true);
+            logBtn.gameObject.SetActive(true);
+            loadBtn.gameObject.SetActive(true);
             autoBtnText.text = "AUTO"; // 버튼 텍스트를 "자동진행"으로 변경합니다.
             CancelInvoke(nameof(HandleMouseClick));
         }
-
     }
 
 
@@ -58,13 +76,8 @@ public class ScriptOutput : MonoBehaviour
         setA.gameObject.SetActive(false);
         setB.gameObject.SetActive(false);
         saveImgCav.gameObject.SetActive(false);
-        nameText.text = names[count];
-        scripts.text = texts[count];
 
-        //state 패턴 적용
-        ScriptOutputState state = GetComponent<ScriptOutputState>();
-        state.SOState(facialExpressions[count], names[count]);
-        count++;
+        HandleMouseClick();
 
         autoBtn.onClick.AddListener(autoBtnClick);
         skipBtn.onClick.AddListener(() =>
@@ -73,12 +86,23 @@ public class ScriptOutput : MonoBehaviour
             HandleMouseClick();
         });
 
+        loadScreen.gameObject.SetActive(false);
+
+        loadBtn.onClick.AddListener(() =>
+        {
+            loadScreen.gameObject.SetActive(true);
+            loadBackBtn.onClick.AddListener(() =>
+            {
+                loadScreen.gameObject.SetActive(false);
+            });
+        });
+
     }
 
     //클릭 시 다음 대사로
     private void Update()
     {
-        if (!scrollRect.gameObject.activeSelf && !backBtn.gameObject.activeSelf && logBtn.gameObject.activeSelf && !isButtonClicked && !loadScreen.gameObject.activeSelf)
+        if (!scrollRect.gameObject.activeSelf && !logbackBtn.gameObject.activeSelf && logBtn.gameObject.activeSelf && !isButtonClicked && !loadScreen.gameObject.activeSelf)
         {
 
             // 마우스 클릭 감지
