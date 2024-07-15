178e348d473400103d28dcd210255f36bf73518a
MJK <jk008532@gmail.com>
2024-07-15 16:37:24 +0900

    🎨 Log update v1
    log perfome updated v1

--- On The Land/Assets/Resources/Scripts/logOutput.cs
+++ On The Land/Assets/Resources/Scripts/logOutput.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using static System.Net.Mime.MediaTypeNames;
 
 public class logOutput : MonoBehaviour
 {
@@ -9,7 +10,8 @@ public class logOutput : MonoBehaviour
     public Button backBtn;
 
     public ScrollRect scrollRect;
-    public Text logText;
+    public GameObject chatItemPrefab; // 대화 항목 프리팹
+    public Transform content; // ScrollRect Content
 
     private ScriptOutput scriptOutput; // ScriptOutput 컴포넌트를 참조하기 위한 변수 추가
 
@@ -24,10 +26,10 @@ public class logOutput : MonoBehaviour
         // ScriptOutput 컴포넌트 가져오기
         scriptOutput = GetComponent<ScriptOutput>();
 
-        // logText가 null이면 자식 오브젝트에서 찾아서 할당
-        if (logText == null)
+        // content가 null이면 자식 오브젝트에서 찾아서 할당
+        if (content == null)
         {
-            logText = GetComponentInChildren<Text>();
+            content = scrollRect.content;
         }
     }
 
@@ -35,18 +37,55 @@ public class logOutput : MonoBehaviour
     {
         scrollRect.gameObject.SetActive(true);
         backBtn.gameObject.SetActive(true);
-        
+
         logBtn.gameObject.SetActive(false);
-        
+
         backBtn.onClick.AddListener(Task2OnClick);
 
-        // ScriptOutput 컴포넌트가 존재하고 logText가 null이 아닌지 확인
-        if (logText != null && scriptOutput != null)
+        // ScriptOutput 컴포넌트가 존재하는지 확인
+        if (scriptOutput != null)
         {
-            // count 변수만큼의 요소들을 logText에 추가
+            // 기존 대화 항목 삭제
+            foreach (Transform child in content)
+            {
+                Destroy(child.gameObject);
+            }
+
+            // count 변수만큼의 요소들을 생성하여 content에 추가
             for (int i = 0; i < scriptOutput.count; i++)
             {
-                logText.text += "\n" + scriptOutput.names[i] + " : " + scriptOutput.texts[i] + "\n";
+                GameObject chatItem = Instantiate(chatItemPrefab, content);
+
+                // 텍스트와 이미지 설정
+                UnityEngine.UI.Text chatText = chatItem.GetComponentInChildren<UnityEngine.UI.Text>();
+                UnityEngine.UI.Image chatImage = chatItem.GetComponentInChildren<UnityEngine.UI.Image>();
+
+                if (chatText != null)
+                {
+                    chatText.text = scriptOutput.names[i] + " : " + scriptOutput.texts[i];
+                }
+
+                if (chatImage != null)
+                {
+                    switch (scriptOutput.names[i])
+                    {
+                        case "안나": //피안화
+                            chatImage.sprite = scriptOutput.pfpImages[0];
+                            break;
+                        case "아이작": //피이삭
+                            chatImage.sprite = scriptOutput.pfpImages[1];
+                            break;
+                        case "아서": //하수호
+                            chatImage.sprite = scriptOutput.pfpImages[2];
+                            break;
+                        case "양소미":
+                            chatImage.sprite = scriptOutput.pfpImages[3];
+                            break;
+                        default: // 그 외 등장인물
+                            chatImage.sprite = scriptOutput.pfpImages[4];
+                            break;
+                    }
+                }
             }
         }
     }
@@ -55,7 +94,7 @@ public class logOutput : MonoBehaviour
     {
         scrollRect.gameObject.SetActive(false);
         backBtn.gameObject.SetActive(false);
-        
+
         logBtn.gameObject.SetActive(true);
     }
 }
